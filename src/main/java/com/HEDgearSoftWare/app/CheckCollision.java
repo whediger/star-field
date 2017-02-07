53a927b95a817cf10f2637099691e361022fe5b3
Wes Hediger <wes.hediger@gmail.com>
2017-02-06 17:38:00 -0700

    proliferate array of enemy ships to rest of code

--- src/main/java/com/HEDgearSoftWare/app/CheckCollision.java
+++ src/main/java/com/HEDgearSoftWare/app/CheckCollision.java
@@ -7,14 +7,16 @@ public class CheckCollision {
 
   public void checkPanel(GamePanel panel) {
 
-    if(!panel.enemyShip.getIsHit()){
-      boolean hit = panel.ship.checkLaserHit(panel.enemyShip.getX(),
-          (panel.enemyShip.getX()
-          + panel.enemyShip.getShipWidth()),
-          panel.enemyShip.getY(),
-          (panel.enemyShip.getY() + panel.enemyShip.getShipHeight()));
-      if(hit) {
-        panel.enemyShip.hit();
+    for (int i = 0; i < panel.enemyShips.length; i++) {
+      if(!panel.enemyShips[i].getIsHit()){
+        boolean hit = panel.ship.checkLaserHit(panel.enemyShips[i].getX(),
+            (panel.enemyShips[i].getX()
+            + panel.enemyShips[i].getShipWidth()),
+            panel.enemyShips[i].getY(),
+            (panel.enemyShips[i].getY() + panel.enemyShips[i].getShipHeight()));
+        if(hit) {
+          panel.enemyShips[i].hit();
+        }
       }
     }
   }
